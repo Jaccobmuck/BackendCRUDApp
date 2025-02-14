9c85d4837956580afe4c2e22721fd38f5adfd95c
Jacob Muck <jmuck@linfield.edu>
2025-02-14 13:15:29 -0800

    updated movie controller

--- 375ProjectVersion1/Controllers/MovieController.cs
+++ 375ProjectVersion1/Controllers/MovieController.cs
@@ -1,6 +1,58 @@
-﻿namespace _375ProjectVersion1.Controllers
+﻿using _375ProjectVersion1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _375ProjectVersion1.Controllers
 {
-    public class MovieController
+    [ApiController]
+    [Route("/[controller]")]
+    public class MovieController: ControllerBase
     {
+        [HttpGet("[action]")]
+        public IActionResult getItems()
+        {
+            try
+            {
+                using(MyContext db = new MyContext())
+                {
+                    List<MovieModel> Movie = db.Movie.ToList();
+
+                    return new ObjectResult(Movie);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Caught an exception: ", e.Message, "Stack Trace: ", e.StackTrace);
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> postItem([FromBody] MovieModel value)
+        {
+            try
+            {
+                using(MyContext db = new MyContext())
+                {
+                    // creates a movie model object to be able to add items to the db
+                    MovieModel model = new MovieModel();
+
+                    // mapping the input value to the model object
+                    model.Title = value.Title;
+                    model.Genre = value.Genre;
+                    model.ReleaseYear = value.ReleaseYear;
+                    model.Director = value.Director;
+
+                    db.Movie.Add(model);
+                    await db.SaveChangesAsync();
+                    
+                    return new ObjectResult(model);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Caught an exception: ", e.Message, "Stack Trace: ", e.StackTrace);
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
--- 375ProjectVersion1/Models/MyContext.cs
+++ 375ProjectVersion1/Models/MyContext.cs
@@ -4,16 +4,16 @@ namespace _375ProjectVersion1.Models
 {
     public class MyContext : DbContext
     {
-        private String connectionString = "Data Source=CS-16\\COMP375JMUCK;Initial Catalog=MovieRental;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        private String connectionString = "Data Source=MSI\\SQLEXPRESS;Initial Catalog=MovieRental;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        // Lab String -> Data Source=CS-16\\COMP375JMUCK;Initial Catalog=MovieRental;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False
+        // Home String -> Data Source=MSI\SQLEXPRESS;Initial Catalog=MovieRental;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False
 
-        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString);
         }
-        public DbSet<MovieModel> Movies { get; set; }   
-        public DbSet<CustomerModel> Customers { get; set; }
-        public DbSet<RentalModel> Rentals { get; set; }  
-
+        public DbSet<MovieModel> Movie { get; set; }   
+        public DbSet<CustomerModel> Customer { get; set; }
+        public DbSet<RentalModel> Rental { get; set; }  
     }
 }
