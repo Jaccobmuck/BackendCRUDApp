2abbf94f16f210e9050e0319d32e64e920b520b8
Jacob Muck <jacobmuck2004@gmail.com>
2025-02-19 10:16:19 -0800

    finished rental controller

--- 375ProjectVersion1/Controllers/RentalController.cs
+++ 375ProjectVersion1/Controllers/RentalController.cs
@@ -60,5 +60,64 @@ namespace _375ProjectVersion1.Controllers
                 return BadRequest(e);   
             }
         }
+        [HttpPut("[action]/{rentalId}")]
+        public async Task<IActionResult> updateItem([FromBody] RentalModel value,Int64 rentalId, Int64 customerId, Int64 movieId)
+        {
+            try
+            {
+                using (MyContext db = new MyContext()) 
+                { 
+                    RentalModel model = await db.Rental.FirstOrDefaultAsync(x => x.RentalId == rentalId);
+                    CustomerModel customer = await db.Customer.FirstOrDefaultAsync(x=> x.CustomerId == customerId);
+                    MovieModel movie = await db.Movie.FirstOrDefaultAsync(x => x.MovieId == movieId);
+
+                    if (model == null || customerId == null || movieId == null)
+                    {
+                        Console.WriteLine("The rental, customer, or movie id does not exist");
+                        return NotFound();
+                    }
+
+                    model.CustomerId = value.CustomerId;
+                    model.MovieId = value.MovieId;
+                    model.RentDate = value.RentDate;
+                    model.ReturnDate = value.ReturnDate;
+
+                    db.Rental.Update(model);
+                    await db.SaveChangesAsync();
+
+                    return new ObjectResult(model);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("caught exception: ", e.Message, "stack trace: ", e.StackTrace);
+                return BadRequest(e);
+            }
+        }
+        [HttpDelete("[action]/{id}")]
+        public async Task<IActionResult> deleteItem(int id)
+        {
+            try
+            {
+                using (MyContext db = new MyContext())
+                {
+                    RentalModel rental = await db.Rental.FirstOrDefaultAsync(x => x.RentalId == id);
+
+                    if (rental == null) 
+                    {
+                        Console.WriteLine("The id entered doesn't exist: ", id);
+                    }
+                    Console.WriteLine("deleting the rental with the id: ", id);
+                    db.Rental.Remove(rental);
+                    await db.SaveChangesAsync();
+                    return Ok();
+                }
+            }
+            catch (Exception e) 
+            { 
+                Console.WriteLine("caught exception: ", e.Message, "stack trace: ", e.StackTrace);
+                return BadRequest(e);
+            }
+        }
     }
 }
